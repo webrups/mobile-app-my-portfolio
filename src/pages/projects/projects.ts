886adbcd3e14aef5633d396aff46c27b5c078d5d
Steffen Witvliet <switvliet@rovecom.nl>
2017-08-31 13:18:08 +0200

    setup classes ts for new pages

---
+++ src/pages/projects/projects.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { Storage } from '@ionic/storage';
+
+@Component({
+  selector: 'page-projects',
+  templateUrl: 'projects.html'
+})
+
+export class ProjectsPage {
+
+
+
+}
---
+++ src/pages/work_experience/work_experience.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { Storage } from '@ionic/storage';
+
+@Component({
+  selector: 'page-work_experience',
+  templateUrl: 'work_experience.html'
+})
+
+export class WorkExperiencePage {
+
+
+
+}
