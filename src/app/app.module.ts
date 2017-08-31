599c7f1b5a33ceb97920137a8c766bfd787c754f
Steffen Witvliet <switvliet@rovecom.nl>
2017-08-31 11:26:49 +0200

    add ionic cloud settings

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,14 @@ import { ListPage } from '../pages/list/list';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+import { CloudSettings, CloudModule } from '@ionic/cloud-angular';
+
+const cloudSettings: CloudSettings = {
+  'core': {
+    'app_id': '2d36772e'
+  }
+};
+
 @NgModule({
   declarations: [
     MyApp,
@@ -18,6 +26,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
+    CloudModule.forRoot(cloudSettings)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
